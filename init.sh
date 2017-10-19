75ada80f2edddccd499831588c55991c0a3da516
Herald Yu <yuhr123@gmail.com>
2017-10-19 22:41:30 +0800

    更新脚本

--- init.sh
+++ init.sh
@@ -4,33 +4,35 @@
 # ${SUBNET} - VPN 子网地址
 # ${IP} - IP 地址
 
-mkdir -p /etc/tinc/${NETNAME}/hosts
+if [ ! -d /etc/tinc/${NETNAME} ]; then
+    mkdir -p /etc/tinc/${NETNAME}/hosts
 
-# 创建配置文件
-touch /etc/tinc/${NETNAME}/tinc.conf
-echo "Name = ${HOSTNAME}" >> /etc/tinc/${NETNAME}/tinc.conf
-echo "Device = /dev/net/tun" >> /etc/tinc/${NETNAME}/tinc.conf
+    # 创建配置文件
+    touch /etc/tinc/${NETNAME}/tinc.conf
+    echo "Name = ${HOSTNAME}" >> /etc/tinc/${NETNAME}/tinc.conf
+    echo "Device = /dev/net/tun" >> /etc/tinc/${NETNAME}/tinc.conf
 
-# 创建主机文件
-touch /etc/tinc/${NETNAME}/hosts/${HOSTNAME}
-echo "Subnet = ${SUBNET}" >> /etc/tinc/${NETNAME}/hosts/${HOSTNAME}
+    # 创建主机文件
+    touch /etc/tinc/${NETNAME}/hosts/${HOSTNAME}
+    echo "Subnet = ${SUBNET}" >> /etc/tinc/${NETNAME}/hosts/${HOSTNAME}
 
-# 创建 tinc-up
-touch /etc/tinc/${NETNAME}/tinc-up
-echo "#!/bin/sh" >> /etc/tinc/${NETNAME}/tinc-up
-echo "ifconfig \$INTERFACE ${IP} netmask 255.255.255.0 " >> /etc/tinc/${NETNAME}/tinc-up
+    # 创建 tinc-up
+    touch /etc/tinc/${NETNAME}/tinc-up
+    echo "#!/bin/sh" >> /etc/tinc/${NETNAME}/tinc-up
+    echo "ifconfig \$INTERFACE ${IP} netmask 255.255.255.0 " >> /etc/tinc/${NETNAME}/tinc-up
 
-# 创建 tinc-down
-touch /etc/tinc/${NETNAME}/tinc-down
-echo "#!/bin/sh" >> /etc/tinc/${NETNAME}/tinc-down
-echo "ifconfig \$INTERFACE down"  >> /etc/tinc/${NETNAME}/tinc-down
+    # 创建 tinc-down
+    touch /etc/tinc/${NETNAME}/tinc-down
+    echo "#!/bin/sh" >> /etc/tinc/${NETNAME}/tinc-down
+    echo "ifconfig \$INTERFACE down"  >> /etc/tinc/${NETNAME}/tinc-down
 
-# 赋予脚本执行权
-chmod +x /etc/tinc/${NETNAME}/tinc-up
-chmod +x /etc/tinc/${NETNAME}/tinc-down
+    # 赋予脚本执行权
+    chmod +x /etc/tinc/${NETNAME}/tinc-up
+    chmod +x /etc/tinc/${NETNAME}/tinc-down
 
-# 生成密钥
-echo | /usr/sbin/tincd -n ${NETNAME} -K4096
-
-# 运行程序
-/usr/sbin/tincd -n -D ${NETNAME}
+    # 生成密钥
+    echo | /usr/sbin/tincd -n ${NETNAME} -K4096
+else
+    # 运行程序
+    /usr/sbin/tincd -n -D ${NETNAME}
+fi
